f2b99865379fe169bdeabb2f41db9ec039ccde3c
Mupiwa <hmupiwa@gmail.com>
2018-07-01 12:54:41 -0400

    Adding Functions Descriptions for Assignment 02

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates a list containing a function to  
+## a) set the value of the matrix
+## b) get the value of the matrix
+## c) set the value of the inverse of the matrix
+## d) get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     inverse <- NULL
@@ -16,7 +17,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## casehSolve returns the inverse of a matrix given the matrix is invertible
+## If inverse is already computed it retrieves it from cache and returns it else it computes 
+## the inverse of the matrix and caches before returning it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
